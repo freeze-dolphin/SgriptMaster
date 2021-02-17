29188e675e4a8d4e46e6acaca191161832eed55d
freeze-dolphin <40729044+freeze-dolphin@users.noreply.github.com>
2021-02-17 19:18:25 +0800

    .

--- src/main/java/io/freeze_dolphin/sgript_master/SgriptMaster.java
+++ src/main/java/io/freeze_dolphin/sgript_master/SgriptMaster.java
@@ -3,46 +3,46 @@ package io.freeze_dolphin.sgript_master;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.IOException;
 
 import io.izzel.taboolib.loader.Plugin;
-import io.izzel.taboolib.loader.PluginBase;
 import io.izzel.taboolib.module.config.TConfig;
 import io.izzel.taboolib.module.dependency.Dependency;
 import io.izzel.taboolib.module.inject.TInject;
 
+import org.bukkit.Bukkit;
+
 @Dependency(maven = "org.codehaus.groovy:groovy-all:3.0.7")
 public class SgriptMaster extends Plugin {
 
-	@TInject("settings.yml")
-	private TConfig conf;
+    @TInject("settings.yml")
+    private TConfig conf;
 
-	@Override
-	public void onEnable() {
+    @Override
+    @SuppressWarnings("CallToPrintStackTrace")
+    public void onEnable() {
 
-		InputStream is = getPlugin().getClass().getClassLoader().getResourceAsStream("/scripts");
-		File f = new File(getPlugin().getDataFolder().getPath() + File.separator + "scripts");
+        InputStream is = getPlugin().getClass().getClassLoader().getResourceAsStream("/scripts");
+        File f = new File(getPlugin().getDataFolder().getPath() + File.separator + "scripts");
 
-		try {
-			if (!f.exists()) {
-				f.mkdirs();
-			}
+        try {
+            if (!f.exists()) {
+                f.mkdirs();
 
-			FileOutputStream fos = new FileOutputStream(f);
+                try (FileOutputStream fos = new FileOutputStream(f)) {
+                    byte[] b = new byte[is.available()];
+                    while (is.read(b) != -1) {
+                        fos.write(b);
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            getPlugin().getLogger().severe("Could not create plugin data, self-disabling...");
+            Bukkit.getPluginManager().disablePlugin(Plugin.getInstance().getPlugin());
+        }
 
-			int ch = 0;
-			while ((ch = is.read()) != -1) {
-				fos.write(ch);
-			}
-
-			fos.close();
-			is.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			getPlugin().getLogger().severe("Could not create plugin data, self-disabling...");
-			PluginBase.setDisabled(true);
-		}
-
-		super.onEnable();
-	}
+        super.onEnable();
+    }
 
 }
